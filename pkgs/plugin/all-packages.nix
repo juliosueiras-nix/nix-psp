520e58a30457913369336b76c35ad606a359a674
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-09-30 02:01:05 -0400

    Update

---
+++ pkgs/plugin/all-packages.nix
@@ -0,0 +1,36 @@
+{ stdenv, zip, pspsdk, libraries, fetchFromGitHub, symlinkJoin , ... }:
+
+let
+  pspsdkEnv = symlinkJoin  {
+    name = "test-env";
+    paths = [
+      pspsdk
+    ];
+  };
+in {
+  usbvideo = stdenv.mkDerivation {
+    name = "uvc";
+
+    buildInputs = [ pspsdkEnv ];
+
+    installPhase = ''
+      mkdir -p $out/{nix-support,plugin}
+      chmod a+rw uvc.prx
+      ${zip}/bin/zip uvc.zip uvc.prx
+      cp uvc.zip $out/
+      echo "file psp-plugin $out/uvc.zip" >> $out/nix-support/hydra-build-products
+    '';
+
+    src = fetchFromGitHub {
+      repo = "psp-uvc-usb-video-class";
+      owner = "xerpi";
+      rev = "master";
+      sha256 = "yoILfJzloNB+J32diykdErJgCXsfJdJRAPmgtYgR77A=";
+    };
+
+    dontStrip = true;
+    dontPatchELF = true;
+    dontDisableStatic = true;
+    hardeningDisable = [ "all" ];
+  };
+}
