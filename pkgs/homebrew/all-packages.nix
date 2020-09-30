881c68d9962f0e3a00bfc062f82fc7bb306f5d8f
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-09-30 02:47:02 -0400

    Update

--- pkgs/homebrew/all-packages.nix
+++ pkgs/homebrew/all-packages.nix
@@ -14,6 +14,19 @@ let
       libraries.SDLPackages.ttf
     ];
   };
+
+  pspsdkEnv2 = symlinkJoin  {
+    name = "test-env";
+    paths = [
+      pspsdk
+      libraries.libpspvram
+      libraries.jpeg
+      libraries.pspgl
+      libraries.zlib
+      libraries.pspirkeyb
+      libraries.libpng
+    ];
+  };
 in {
   gameOfLife = stdenv.mkDerivation {
     name = "game_of_life_for_psp_with_sdl";
@@ -37,6 +50,39 @@ in {
       owner = "Hettomei";
       rev = "8346579a9975f41763b2381cfb601900a9a84e6e";
       sha256 = "tz96Pf4RS2L1wcJ2HYr4+dltHdzTNi3YNcrqhLOXIjQ=";
+    };
+
+    dontStrip = true;
+    dontPatchELF = true;
+    dontDisableStatic = true;
+    hardeningDisable = [ "all" ];
+  };
+
+  dungeon = stdenv.mkDerivation {
+    name = "Simple-Dungeon-Generator-PSP";
+
+    PSPDEV = "${pspsdkEnv2}";
+
+    buildInputs = [ pspsdkEnv2 ];
+
+    patchPhase = ''
+      sed -i 's;INCDIR += ./include;INCDIR += ./include ${pspsdkEnv2}/psp/include;g' Makefile
+      sed -i 's;-L''${PSPDEV}/psp;-L''${PSPDEV}/psp/lib;g' Makefile
+    '';
+
+    installPhase = ''
+      mkdir -p $out/nix-support
+      cd bin
+      ${zip}/bin/zip dungeon.zip EBOOT.PBP
+      cp dungeon.zip $out/
+      echo "file psp-homebrew $out/dungeon.zip" >> $out/nix-support/hydra-build-products
+    '';
+
+    src = fetchFromGitHub {
+      repo = "Simple-Dungeon-Generator-PSP";
+      owner = "stacksta";
+      rev = "e96e41743e828434f4fc8f41a50a3bba17cb6aca";
+      sha256 = "pD8RxXPJEY8By/naC/5UBnDVTVbdTTLWJsPe1iqh0mY=";
     };
 
     dontStrip = true;
