939d0c93ac654ba640876c54102f27e8676f266e
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-10-10 22:18:30 -0400

    Update

--- release.nix
+++ release.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = (<nixpkgs>);
   nixpkgs-mozilla = (<nixpkgs-mozilla>);
 in {
-  test = (builtins.getFlake("git+https://github.com/juliosueiras-nix/nix-psp")).outputs.hydraJobs;
+  test = (builtins.getFlake("git+https://github.com/juliosueiras-nix/nix-psp/master")).outputs.hydraJobs;
   #test = (import <src/flake.nix>).outputs {
   #  inherit nixpkgs nixpkgs-mozilla;
   #};
