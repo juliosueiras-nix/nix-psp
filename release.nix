0081bc84386071f930c9818b29633a22e0d03003
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-10-10 23:20:21 -0400

    Update

--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 let
   src = fetchGit {
     url = "https://github.com/juliosueiras-nix/nix-psp";
-    ref = "master";
+    ref = "refs/heads/master";
   };
 in {
   test = (builtins.getFlake("git+https://github.com/juliosueiras-nix/nix-psp?ref=${src.rev}")).outputs.hydraJobs;
