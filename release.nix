0482030bdf3746b00f5e9d1827e20bae25bd12cd
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-10-10 21:56:53 -0400

    Update

--- release.nix
+++ release.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = (<nixpkgs>);
   nixpkgs-mozilla = (<nixpkgs-mozilla>);
 in {
-  test = builtins.getFlake(<src>);
+  test = builtins.getFlake("${<src>}");
   #test = (import <src/flake.nix>).outputs {
   #  inherit nixpkgs nixpkgs-mozilla;
   #};
